158172e1003e079f50f7eca3cd4498984335d711
sariold <sarioldrenzo@gmail.com>
2022-11-10 21:22:12 -0600

    refactor: Configured Truffle for test deployment
    Merging to fix missing files from main.

--- contracts/MaticStone.sol
+++ contracts/MaticStone.sol
@@ -7,12 +7,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract TestMTS is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
+contract MaticStone is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("TestMTS", "TMTS") {}
+    constructor() ERC721("MaticStone", "MTS") {}
 
     function safeMint(address to, string memory uri) public {
         uint256 tokenId = _tokenIdCounter.current();
